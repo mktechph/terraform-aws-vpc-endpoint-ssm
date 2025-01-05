a2d418a571de783a13a06875de261f8c5c553008
mktechph <114472493+mktechph@users.noreply.github.com>
2025-01-05 13:12:59 +0800

    Security gruop and private dns enabled.

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@ resource "aws_vpc_endpoint" "ssm_endpoint" {
   service_name      = "com.amazonaws.ap-southeast-1.ssm"
   vpc_endpoint_type = "Interface"
 
-  subnet_ids = var.ssm_endpoint_subnet_id
-  #security_group_ids = [
-  #  aws_security_group.sg1.id,
-  #]
+  subnet_ids         = var.ssm_endpoint_subnet_id
+  security_group_ids = var.sg_ssm_endpoint
+
+  private_dns_enabled = true
+
   tags = var.endpoint_tags
 }
 
@@ -15,10 +16,11 @@ resource "aws_vpc_endpoint" "ssm_messages_endpoint" {
   service_name      = "com.amazonaws.ap-southeast-1.ssmmessages"
   vpc_endpoint_type = "Interface"
 
-  subnet_ids = var.ssm_messages_subnet_id
-  #security_group_ids = [
-  #  aws_security_group.sg1.id,
-  #]
+  subnet_ids         = var.ssm_messages_subnet_id
+  security_group_ids = var.sg_ssm_messages
+
+  private_dns_enabled = true
+
   tags = var.endpoint_tags
 }
 
@@ -27,9 +29,10 @@ resource "aws_vpc_endpoint" "ec2_messages_endpoint" {
   service_name      = "com.amazonaws.ap-southeast-1.ec2messages"
   vpc_endpoint_type = "Interface"
 
-  subnet_ids = var.ec2_messages_subnet_id
-  #security_group_ids = [
-  #  aws_security_group.sg1.id,
-  #]
+  subnet_ids         = var.ec2_messages_subnet_id
+  security_group_ids = var.sg_ec2_messages
+
+  private_dns_enabled = true
+
   tags = var.endpoint_tags
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "vpc_id" {
   description = "The VPC ID where the SSM Endpoint will be deployed."
-  type = string
+  type        = string
 }
 
 variable "endpoint_tags" {
   description = "Tags for the SSM Endpoint."
-  type = map(string)
+  type        = map(string)
 }
 
 
@@ -13,15 +13,30 @@ variable "endpoint_tags" {
 
 variable "ssm_endpoint_subnet_id" {
   description = "The Subnet ID(s) where the SSM Endpoint will be deployed."
-  type = list(string)
+  type        = list(string)
 }
 
 variable "ssm_messages_subnet_id" {
   description = "The Subnet ID(s) where the SSM Messages Endpoint will be deployed."
-  type = list(string)
+  type        = list(string)
 }
 
 variable "ec2_messages_subnet_id" {
   description = "The Subnet ID(s) where the EC2 Messages Endpoint will be deployed."
-  type = list(string)
+  type        = list(string)
+}
+
+variable "sg_ssm_endpoint" {
+  description = "The Security Group ID(s) for the Endpoint."
+  type        = list(string)
+}
+
+variable "sg_ssm_messages" {
+  description = "The Security Group ID(s) for the Endpoint."
+  type        = list(string)
+}
+
+variable "sg_ec2_messages" {
+  description = "The Security Group ID(s) for the Endpoint."
+  type        = list(string)
 }
